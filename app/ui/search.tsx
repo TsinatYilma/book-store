30de605b68c4ba8a27079fb1f96f238d9f19f0e9
Tsinat <tsinatyilma16@gmail.com>
2025-07-23 22:03:57 +0300

    deployment

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,6 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  /*
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 3000)
+  }, 3000)*/
   return (
     <div className=" flex gap-4 w-full justify-center ">
       <div className=" relative ">
@@ -29,9 +30,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className=" peer block rounded-md py-[4px] sm:py-[6px] pl-10 text-sm outline-2 placeholder:text-gray-500 sm:max-w-[450px] md:w-[850px] focus:outline-[#0AA1A1]"
         placeholder={placeholder}
         onChange={(e)=>{
-          handleSearch(e.target.value)
+          //handleSearch(e.target.value)
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        //defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
       </div>
