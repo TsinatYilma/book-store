7d8d111aa9cde135d75118810e25eac2ac4d844d
Tsinat <tsinatyilma16@gmail.com>
2025-07-23 21:59:32 +0300

    for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center ">
       <header className="flex p-4 pl-8 pr-8 w-full justify-between items-center mb-18">
-         <div className="flex gap-4">
+         <div className="flex gap-6">
            <Link href="/">Home</Link>
            <Link href="/genres">Genres</Link>
            <Link href="/blog">Blog</Link>
@@ -15,7 +15,7 @@ export default function Home() {
          <div className="">
           <h1 className="text-2xl font-bold">Book Store</h1>
          </div>
-         <div className="flex gap-4  items-center">
+         <div className="flex gap-6  items-center">
            <Link href="/login">Login</Link>
            <p className="font-bold text-2xl flex items-center">|</p>
            <Link className="text-[#238E8E]" href="/signup">Register</Link>
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,7 @@
 import {Gabarito, Handlee} from "next/font/google";
 
 export const gabarito = Gabarito({
+  subsets: ['latin'],
   weight: ['400', '700', '800'], })
 
 export const handlee = Handlee({
