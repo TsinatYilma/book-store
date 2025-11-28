55175582f2618d79d409d429608e210cce283ee0
Tsinat <tsinatyilma16@gmail.com>
2025-11-28 23:15:42 +0300

    user data in the admin page

--- app/admin/user/(usertable)/page.tsx
+++ app/admin/user/(usertable)/page.tsx
@@ -1,9 +1,17 @@
 "use client"
 import Image from 'next/image'
 import { MagnifyingGlassIcon, UserIcon, PlusCircleIcon, FunnelIcon } from '@heroicons/react/24/outline';
-
+import { useQuery } from '@tanstack/react-query';
+import {fetchAllUsers} from "@/app/lib/fetching-data"
+import {User} from "@/app/lib/definition"
 
 export default function Page({ placeholder }: { placeholder: string }) {
+
+  const { data: users, isLoading, error } = useQuery<User[], Error>({
+    queryKey: ["users"],
+    queryFn: fetchAllUsers,
+  });
+
   return (
   <div className="">
     <table className="table-auto text-lg text-gray-300  w-full gap-10">
@@ -19,20 +27,20 @@ export default function Page({ placeholder }: { placeholder: string }) {
       
       <tbody className='pt-8'>
         {
-          [...Array(9)].map((index)=>(
+          users?.map((user)=>(
             
-            <tr key={index} className='text-sm text-gray-400'>
+            <tr key={user.id} className='text-sm text-gray-400'>
               <td className=" px-4 py-2  flex justify-center">
                 <UserIcon className='h-[25px] w-[25px] text-cyan-400'/>
               </td>
               <td className=" px-4 py-2 text-center">
-                <p className="">Jenna Fischer</p>
+                <p className="">{user.name}</p>
               </td>
               <td className=" px-4 py-2 text-center ">
-                <p className="">jennaFischer@gmail.com</p>
+                <p className="">-</p>
               </td>
               <td className=" px-4 py-2 text-center">
-                <p className="|">0938293829</p>
+                <p className="|">{user.email}</p>
               </td>
               <td className=" px-4 py-2 flex gap-2 justify-center ">
                 <Image src={'/online.png'} alt=' ' width={25} height={2}/>
