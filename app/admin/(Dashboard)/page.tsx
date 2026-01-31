0b7ef1f2798d80494aa84ba759e2608a4ba6496b
Medan <medanabebe17@gmail.com>
2026-01-31 20:05:55 +0300

    this too needto be ed saved

--- app/admin/(Dashboard)/page.tsx
+++ app/admin/(Dashboard)/page.tsx
@@ -32,7 +32,7 @@ export default function AdminPage() {
           <div className="flex gap-4 mt-8">
               <div className="bg-black/20 p-4 rounded-lg flex flex-col gap-2 items-center ">
                     <div className="flex gap-2 items-center">
-                      <BookOpenIcon className='h-[20px] w-[20px] text-gray-200'/>
+                      <BookOpenIcon className='h-5 w-5 text-gray-200'/>
                     <p className="text-3xl font-gantari">{counts?.count.books}</p>
                     </div>
                     <h1 className="text-[16px] text-gray-600">Total Books</h1>
@@ -40,21 +40,21 @@ export default function AdminPage() {
                 </div>
                 <div className="bg-black/20 p-4 rounded-lg flex flex-col gap-2 items-center ">
                       <div className="flex gap-2 items-center">
-                          <UserGroupIcon className='h-[20px] w-[20px] text-gray-200'/>
+                          <UserGroupIcon className='h-5 w-5 text-gray-200'/>
                         <p className="text-3xl font-gantari">{counts?.count.users}</p>
                         </div>
                       <h1 className="text-[16px] text-gray-600">Total Users</h1>
                 </div>
                 <div className="bg-black/20 p-4 rounded-lg flex flex-col gap-2 items-center">
                     <div className="flex gap-2 items-center">
-                        <SparklesIcon className='h-[20px] w-[20px] text-gray-200'/>
+                        <SparklesIcon className='h-5 w-5 text-gray-200'/>
                       <p className="text-3xl font-gantari">567</p>
                       </div>
                       <h1 className="text-[16px] text-gray-600">New Books</h1>
                     </div>
                     <div className="bg-black/20 p-4 rounded-lg flex flex-col gap-2 items-center">
                       <div className="flex gap-2 items-center">
-                        <UserPlusIcon className='h-[20px] w-[20px] text-gray-200'/>
+                        <UserPlusIcon className='h-5 w-5 text-gray-200'/>
                       <p className="text-3xl font-gantari">67</p>
                       </div>
                       <h1 className="text-[16px] text-gray-600 ">New Users</h1>
@@ -64,30 +64,30 @@ export default function AdminPage() {
                   <div className="flex flex-col  max-w-[650px]">
                         <div className="flex">
                           <div className="flex flex-wrap justify-center gap-5 my-12 ">
-                            <div className="w-[183px] h-[176px] rounded-lg bg-gradient-to-tr from-black to-cyan-900 pl-1 flex flex-col justify-start gap-6 sm:gap-14 border-[0.15] border-gray-700">
+                            <div className="w-[183px] h-44 rounded-lg bg-linear-to-tr from-black to-cyan-900 pl-1 flex flex-col justify-start gap-6 sm:gap-14 border-[0.15] border-gray-700">
                               <div className="p-2 w-fit bg-transparent">
-                                  <FaceSmileIcon className='h-[24px] w-[24px] text-cyan-600 '/>
+                                  <FaceSmileIcon className='h-6 w-6 text-cyan-600 '/>
                               </div>
                               <div className="">
-                                  <h1 className="font-bold text-lg text-transparent bg-clip-text bg-gradient-to-b from-cyan-200 to-white">17 Reviews</h1>
+                                  <h1 className="font-bold text-lg text-transparent bg-clip-text bg-linear-to-b from-cyan-200 to-white">17 Reviews</h1>
                                   <p className="text-gray-500 text-[11px]">17 Reviews are waiting approval</p>
                               </div>
                             </div>
-                            <div className="w-[183px] h-[176px] rounded-lg bg-gradient-to-tr from-black to-cyan-900 pl-1 flex flex-col justify-start gap-6 sm:gap-14 border-[0.15] border-gray-700">
+                            <div className="w-[183px] h-44 rounded-lg bg-linear-to-tr from-black to-cyan-900 pl-1 flex flex-col justify-start gap-6 sm:gap-14 border-[0.15] border-gray-700">
                                       <div className="p-2 w-fit bg-transparent">
-                                          <ChatBubbleLeftIcon className='h-[24px] w-[24px] text-cyan-600 '/>
+                                          <ChatBubbleLeftIcon className='h-6 w-6 text-cyan-600 '/>
                                       </div>
                                       <div className="">
-                                          <h1 className="font-bold text-lg text-transparent bg-clip-text bg-gradient-to-b from-cyan-200 to-white">5 Messages</h1>
+                                          <h1 className="font-bold text-lg text-transparent bg-clip-text bg-linear-to-b from-cyan-200 to-white">5 Messages</h1>
                                           <p className="text-gray-500 text-[11px]">You have recieved 5 messages</p>
                                       </div>
                             </div>
-                            <div className="w-[183px] h-[176px] rounded-lg bg-gradient-to-tr from-black to-cyan-900 pl-1 flex flex-col justify-start gap-6 sm:gap-14 border-[0.15] border-gray-700">
+                            <div className="w-[183px] h-44 rounded-lg bg-linear-to-tr from-black to-cyan-900 pl-1 flex flex-col justify-start gap-6 sm:gap-14 border-[0.15] border-gray-700">
                                     <div className="p-2 w-fit bg-transparent">
-                                          <IdentificationIcon className='h-[24px] w-[24px] text-cyan-600 '/>
+                                          <IdentificationIcon className='h-6 w-6 text-cyan-600 '/>
                                       </div>
                                       <div className="">
-                                          <h1 className="font-bold text-lg text-transparent bg-clip-text bg-gradient-to-b from-cyan-200 to-white">{counts?.count.author} Authors</h1>
+                                          <h1 className="font-bold text-lg text-transparent bg-clip-text bg-linear-to-b from-cyan-200 to-white">{counts?.count.author} Authors</h1>
                                           <p className="text-gray-500 text-[11px]">2 Authors are waiting approval</p>
                                       </div>
                           </div>
